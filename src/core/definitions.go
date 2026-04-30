6c6d25992b7baab458f996e780032619659b455e
KiamMota <somentekiam@gmail.com>
2026-04-29 23:45:37 -0300

    adding setter and getter in core

--- src/core/definitions.go
+++ src/core/definitions.go
@@ -9,9 +9,63 @@ const (
 func MacroTable(macro string) string {
 	switch macro {
 	case "pwd":
-		return InterState.CurrentDirectory
+		return InterState.GetCurrentDirectory()
 	case "home":
-		return InterState.UserHomeDir
+		return InterState.GetUserHomeDirectory()
 	default: return macro
 	}
 }
+
+const (
+	TypeDirectory = "Directory"
+	TypeUnknown   = "Unknown"
+
+	TypeGolang     = "Golang"
+	TypeC          = "C"
+	TypeCPP        = "C++"
+	TypeJS         = "JavaScript"
+	TypeTS         = "TypeScript"
+	TypePython     = "Python"
+	TypeJava       = "Java"
+	TypeRust       = "Rust"
+	TypePHP        = "PHP"
+	TypeRuby       = "Ruby"
+	TypeCSharp     = "C#"
+
+	TypeText       = "Text"
+	TypeMarkdown   = "Markdown"
+	TypeJSON       = "JSON"
+	TypeXML        = "XML"
+	TypeYAML       = "YAML"
+	TypeTOML       = "TOML"
+
+	TypeHTML       = "HTML"
+	TypeCSS        = "CSS"
+
+	TypePNG        = "PNG"
+	TypeJPEG       = "JPEG"
+	TypeGIF        = "GIF"
+	TypeWEBP       = "WebP"
+	TypeSVG        = "SVG"
+
+	TypeMP3        = "MP3"
+	TypeWAV        = "WAV"
+	TypeOGG        = "OGG"
+
+	TypeMP4        = "MP4"
+	TypeMKV        = "MKV"
+	TypeAVI        = "AVI"
+
+	TypeZIP        = "ZIP Archive"
+	TypeRAR        = "RAR Archive"
+	TypeTAR        = "TAR Archive"
+	TypeGZ         = "GZIP Archive"
+
+	TypeEXE        = "Executable"
+	TypeBIN        = "Binary"
+	TypeSO         = "Linux Shared Library"
+	TypeDLL        = "DLL"
+
+	TypeCSV        = "CSV"
+	TypeDatabase   = "Database"
+)
--- src/core/fsio.go
+++ src/core/fsio.go
@@ -9,16 +9,16 @@ import (
 )
 
 type FileMetadata struct {
-	Name string
-	Absolute string
-	Size string 
-	ModifiedAt string
-	IsDirectory string
+	Name        string
+	Type        string
+	Size        string
+	ModifiedAt  string
 	Permissions string
+	Absolute    string
 }
 
-func Put(name string, param string) (error) {
-	if(strings.Contains(name, "/")){
+func Put(name string, param string) error {
+	if strings.Contains(name, "/") {
 
 		return (fsIoDirCreate(name))
 	}
@@ -27,7 +27,7 @@ func Put(name string, param string) (error) {
 }
 
 func Remove(name string) error {
-	if fsIoExists(name){
+	if fsIoExists(name) {
 		return os.RemoveAll(name)
 	}
 	return errors.New("file doenst exists")
@@ -35,33 +35,34 @@ func Remove(name string) error {
 
 func Get(name string) ([]FileMetadata, error) {
 
-	if name == "./"{
+	if name == "./" {
 		return fsIoListDir(InterState.CurrentDirectory)
 	}
 
-	if(!fsIoExists(name)){
-		return nil, nil 
- 	 }
+	if !fsIoExists(name) {
+		return nil, nil
+	}
 
 	handle := []FileMetadata{}
 
 	fs, e := EntryInspect(name)
-	if e != nil { return nil, e} 
+	if e != nil {
+		return nil, e
+	}
 	return append(handle, fs), nil
 
 }
 
 func fsIoExists(filePath string) bool {
 	_, err := os.Stat(filePath)
-	if err != nil{
-		return false 
+	if err != nil {
+		return false
 	}
-		return true
+	return true
 }
 
-
 func fsIoFileCreate(filename string) (*os.File, error) {
-    return os.Create(filename)
+	return os.Create(filename)
 }
 
 func fsIoDirCreate(filename string) error {
@@ -74,13 +75,11 @@ func fsIoDirCreate(filename string) error {
 
 func fsIoDeleteEntry(name string) error {
 	err := os.Remove(name)
-	if err != nil { 
+	if err != nil {
 		return err
 	}
 	return nil
 }
-
-
 
 func NormalizePath(base, input string) string {
 	var p string
@@ -97,22 +96,24 @@ func NormalizePath(base, input string) string {
 func fsIoAppendFile(name string, content string) error {
 	f, err := os.OpenFile(name, os.O_APPEND|os.O_CREATE|os.O_RDONLY, 0644)
 	if err != nil {
-		return err 
+		return err
 	}
 	defer f.Close()
 
-	if _, err := f.WriteString(content); err != nil { return err }
+	if _, err := f.WriteString(content); err != nil {
+		return err
+	}
 	return nil
 }
 
-func fsIoListDir(name string) ([]FileMetadata, error){
-	
+func fsIoListDir(name string) ([]FileMetadata, error) {
+
 	e, err := os.Stat(name)
 	if err != nil {
 		return nil, err
 	}
-	if(!e.IsDir()) {
-		return nil, errors.New("this is not dir") 
+	if !e.IsDir() {
+		return nil, errors.New("this is not dir")
 	}
 	entries, err := os.ReadDir(name)
 	if err != nil {
@@ -120,13 +121,10 @@ func fsIoListDir(name string) ([]FileMetadata, error){
 	}
 	handle := make([]FileMetadata, 0, len(entries))
 
-
-
 	for _, e := range entries {
-		meta,_ := EntryInspect(e.Name())
+		meta, _ := EntryInspect(e.Name())
 
 		handle = append(handle, meta)
-			
 
 	}
 	return handle, nil
@@ -138,18 +136,125 @@ func EntryInspect(name string) (FileMetadata, error) {
 		return FileMetadata{}, err
 	}
 
-	var isDir string 
 	abs, err := filepath.Abs(name)
-	if err != nil { abs = ""}
-	if f.IsDir() {
-		isDir = StrTrue 
+	if err != nil {
+		abs = ""
 	}
 
+
 	return FileMetadata{
-		Name:    f.Name(),
-		Absolute: abs,
-		Size:    strconv.FormatInt(f.Size(), 10),
-		IsDirectory:   isDir,
-		ModifiedAt: f.ModTime().String(),
+		Name:        f.Name(),
+		Type: fileTypeEntry(name),
+		Absolute:    abs,
+		Size:        strconv.FormatInt(f.Size(), 10),
+		ModifiedAt:  f.ModTime().String(),
 	}, nil
+}
+
+func fileTypeEntry(filePath string) string {
+	info, err := os.Stat(filePath)
+	if err != nil {
+		return "Unknown"
+	}
+
+	if info.IsDir() {
+		return "Directory"
+	}
+
+	ext := strings.ToLower(filepath.Ext(filePath))
+
+	switch ext {
+
+	case ".go":
+		return "Golang"
+	case ".c", ".h":
+		return "C"
+	case ".cpp", ".cc", ".cxx", ".hpp":
+		return "C++"
+	case ".js":
+		return "JavaScript"
+	case ".ts":
+		return "TypeScript"
+	case ".py":
+		return "Python"
+	case ".java":
+		return "Java"
+	case ".rs":
+		return "Rust"
+	case ".php":
+		return "PHP"
+	case ".rb":
+		return "Ruby"
+	case ".cs":
+		return "C#"
+
+	case ".txt":
+		return "Text"
+	case ".md":
+		return "Markdown"
+	case ".json":
+		return "JSON"
+	case ".xml":
+		return "XML"
+	case ".yaml", ".yml":
+		return "YAML"
+	case ".toml":
+		return "TOML"
+
+	case ".html", ".htm":
+		return "HTML"
+	case ".css":
+		return "CSS"
+
+	case ".png":
+		return "PNG Image"
+	case ".jpg", ".jpeg":
+		return "JPEG"
+	case ".gif":
+		return "GIF"
+	case ".webp":
+		return "WebP"
+	case ".svg":
+		return "SVG"
+
+	case ".mp3":
+		return "MP3"
+	case ".wav":
+		return "WAV"
+	case ".ogg":
+		return "OGG"
+
+	case ".mp4":
+		return "MP4"
+	case ".mkv":
+		return "MKV" 
+	case ".avi":
+		return "AVI"
+
+	case ".zip":
+		return "ZIP Archive"
+	case ".rar":
+		return "RAR Archive"
+	case ".tar":
+		return "TAR Archive"
+	case ".gz":
+		return "GZIP Archive"
+
+	case ".exe":
+		return "Executable"
+	case ".bin":
+		return "Binary"
+	case ".so":
+		return "Shared Library"
+	case ".dll":
+		return "DLL"
+
+	case ".csv":
+		return "CSV"
+	case ".db", ".sqlite":
+		return "Database"
+
+	default:
+		return "Unknown"
+	}
 }
